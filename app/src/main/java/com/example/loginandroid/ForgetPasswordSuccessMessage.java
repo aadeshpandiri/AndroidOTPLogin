62e1d0313f5349cfceae00988779304907e1c5e6
aadeshpandiri <aadeshpandiri@gmail.com>
2021-04-28 20:26:32 +0530

    Initial commit

---
+++ app/src/main/java/com/example/loginandroid/ForgetPasswordSuccessMessage.java
@@ -0,0 +1,14 @@
+package com.example.loginandroid;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ForgetPasswordSuccessMessage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password_success_message);
+    }
+}
